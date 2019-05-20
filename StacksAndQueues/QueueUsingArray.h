28961a60e22a9b7970a74258ca92bf44b9c7b5d9
Bartleby <bartleby2718@gmail.com>
2019-05-20 19:28:00 +0900

    Implement queue using array
    template class, constructor delegation, const-qualification, error handling...

---
+++ StacksAndQueues/QueueUsingArray.h
@@ -0,0 +1,135 @@
+//
+// Created by bartl on 05/20/2019.
+//
+
+#ifndef STACKSANDQUEUES_QUEUEUSINGARRAY_H
+#define STACKSANDQUEUES_QUEUEUSINGARRAY_H
+
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+template<class T>
+class QueueUsingArray
+{
+    /**
+     * Implementation of stack of fixed size using an array
+     **/
+    T *array;
+    size_t headIndex;
+    size_t tailIndex;
+    size_t capacity;  // capacity of the stack (= capacity of the array - 1)
+public:
+    explicit QueueUsingArray(size_t capacity);
+
+    QueueUsingArray() : QueueUsingArray(10) {}
+
+    ~QueueUsingArray();
+
+    void enqueue(T element);
+
+    T dequeue();
+
+    bool empty() const;
+
+    bool full() const;
+
+    void print() const;
+};
+
+template<class T>
+QueueUsingArray<T>::QueueUsingArray(const size_t capacity)
+{
+    if (capacity >= numeric_limits<size_t>::max())
+    {
+        throw out_of_range("Try a smaller capacity.");
+    }
+    else
+    {
+        array = new T[capacity + 1];  // space before head is always kept empty
+        headIndex = 0;  // index of the first element
+        tailIndex = 0;  // index after the last element
+        this->capacity = capacity;
+        clog << "Created a queue of capacity " << capacity << "." << endl;
+    }
+}
+
+template<class T>
+QueueUsingArray<T>::~QueueUsingArray()
+{
+    delete array;
+    clog << "Destroyed a queue of capacity " << capacity << "." << endl;
+}
+
+template<class T>
+void QueueUsingArray<T>::enqueue(const T element)
+{
+    if (full())
+    {
+        string message = string("Queue Overflow: The queue is full.")
+                         + " Failed to enqueue " + to_string(element) + ".";
+        throw out_of_range(message);
+    }
+    else
+    {
+        array[tailIndex++] = element;
+        tailIndex %= (capacity + 1);
+    }
+}
+
+template<class T>
+T QueueUsingArray<T>::dequeue()
+{
+    if (empty())
+    {
+        string message = string("Queue Underflow: The queue is empty.")
+                         + " Failed to dequeue.";
+        throw out_of_range(message);
+    }
+    else
+    {
+        T element = array[headIndex++];
+        headIndex %= (capacity + 1);
+        return element;
+    }
+}
+
+template<class T>
+bool QueueUsingArray<T>::full() const
+{
+    return (tailIndex + 1) % (capacity + 1) == headIndex;
+}
+
+template<class T>
+bool QueueUsingArray<T>::empty() const
+{
+    return headIndex == tailIndex;
+}
+
+template<class T>
+void QueueUsingArray<T>::print() const
+{
+    if (empty())
+    {
+        clog << "The queue is currently empty." << endl;
+    }
+    else
+    {
+        clog << "Elements in the queue: ";
+        for (size_t current = headIndex; current != tailIndex; (++current) %= (capacity + 1))
+        {
+            clog << to_string(array[current]) << " ";
+        }
+        if (full())
+        {
+            clog << "(The queue is currently full.)";
+        }
+        clog << endl;
+    }
+}
+
+#endif //STACKSANDQUEUES_QUEUEUSINGARRAY_H
--- StacksAndQueues/main.cpp
+++ StacksAndQueues/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include "QueueUsingArray.h"
 #include "StackUsingArray.h"
 
 using namespace std;
 
 int main()
 {
-
     // #10.1-1 stack using array (default constructor)
     StackUsingArray<int> s1;
     s1.push(4);
@@ -47,7 +47,63 @@ int main()
         cerr << e.what() << endl;
     }
 
-    // TODO: queue using array
+    // #10.1-3 queue using array
+    QueueUsingArray<int> q1(3);
+    q1.enqueue(4);
+    q1.print();
+    q1.enqueue(1);
+    q1.print();
+    q1.enqueue(3);
+    q1.print();
+    q1.dequeue();
+    q1.print();
+    q1.enqueue(8);
+    q1.print();
+    q1.dequeue();
+    q1.print();
+
+    // #10.1-4 queue detecting overflow & underflow
+    QueueUsingArray<int> q2(3);
+    try
+    {
+        q2.dequeue();
+    } catch (out_of_range &e)
+    {
+        cerr << e.what() << endl;
+    }
+    q2.enqueue(5);
+    q2.print();
+    q2.enqueue(6);
+    q2.print();
+    q2.enqueue(7);
+    q2.print();
+    try
+    {
+        q2.enqueue(8);
+    } catch (out_of_range &e)
+    {
+        cerr << e.what() << endl;
+    }
+    q2.dequeue();
+    q2.print();
+    q2.dequeue();
+    q2.print();
+    q2.dequeue();
+    q2.print();
+    try
+    {
+        q2.dequeue();
+    } catch (out_of_range &e)
+    {
+        cerr << e.what() << endl;
+    }
+    q2.enqueue(9);
+    q2.print();
+    q2.enqueue(10);
+    q2.print();
+    q2.enqueue(11);
+    q2.print();
+
     // TODO: queue detecting underflow/overflow
     // TODO: deque using array
     // TODO: queue using two stacks
